068c414e6a03e2fb678a418d4f11017654036de8
Oliwia Gierczak <oliwia.giercza@gmail.com>
2023-02-07 00:25:49 +0100

    done with small help

--- The-collatz-conjecture.c
+++ The-collatz-conjecture.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <cs50.h>
 #include <math.h>
+
+int collatz(int n);
+
 int main(void) 
 {
 
@@ -8,5 +11,17 @@ int main(void)
 
 int collatz(int n) 
 {
+    if (n === 1) 
+    {
+        return;
+    }
+    else if ((n % 2) === 0)
+    {
+        return 1 + (collatz(n/2));
 
+    }
+    else 
+    {
+        return 1 + (collatz(n*3+1))
+    }
 }
